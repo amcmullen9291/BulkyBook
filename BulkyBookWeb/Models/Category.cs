6e8f2c38a92b7bbf18809a6b549cbf2b694c0fd4
amcmullen9291 <amcmullen9291@gmail.com>
2022-02-12 00:40:29 -0500

    Entity annotations. null:false and Primary key

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 namespace BulkyBookWeb.Models;
 
 public class Categroy
 { // notice curlybrace starts a new line?
 
+
+    [Key]    //Don't forget to import the Library. Sets column as Primary key (Identity Column)
     public int Id { get; set; } //keyword 'prop' + enterKey
+    [Required]    //Same as null:false;
     public string Name { get; set; } = "guest";
     public int DisplayOrder { get; set; }
     public DateTime CreateDateTime { get; set; } = DateTime.Now; //sets default
