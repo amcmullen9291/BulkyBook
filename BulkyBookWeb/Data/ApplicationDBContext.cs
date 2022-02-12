ebd85cdd4e73cc910bd6d16c5cf99d89527a6f22
amcmullen9291 <amcmullen9291@gmail.com>
2022-02-12 04:11:09 -0500

    added constructor to ApplicationDb

--- BulkyBookWeb/Data/ApplicationDBContext.cs
+++ BulkyBookWeb/Data/ApplicationDBContext.cs
@@ -3,9 +3,14 @@ using BulkyBookWeb.Models;
 namespace BulkyBookWeb.Data;
 public class ApplicationDbContext :DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //keyword 'ctor'
-    {
+    // public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //keyword 'ctor'
+    // {
         
+    // }
+
+    public ApplicationDbContext(DbSet<Category> categories)
+    {
+        Categories = categories;
     }
 
     public DbSet<Category> Categories { get; set; } //the model and table name
