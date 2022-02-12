78148e06c2d95f00f200ed7ab4f114d44a21ed56
amcmullen9291 <amcmullen9291@gmail.com>
2022-02-12 02:39:31 -0500

    Program.cs Configuration.GetConnectionString

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,7 +1,12 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+)); //dont forget to import packages
 
 var app = builder.Build();
 
